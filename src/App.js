330695865f3e92a881f15dfb559fa1e3b59ac946
Danilo Giarlini <danilogiarlini@gmail.com>
2023-10-16 17:33:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="score-card">
+        <p>Your Result</p>
+        <div className="score-circle">
+          <h1>76</h1>
+          <p>of 100</p>
+        </div>
+        <div>
+          <h2>Great</h2>
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Summary</h2>
+        <div className="reaction">
+          <p>Reaction</p> <p>80</p> <p>/ 100</p>
+        </div>
+        <div className="memory">
+          <p>Memory</p> <p>92</p> <p>/ 100</p>
+        </div>
+        <div className="verbal">
+          <p>Verbal</p> <p>61</p> <p>/ 100</p>
+        </div>
+        <div className="visual">
+          <p>Visual</p> <p>72</p> <p>/ 100</p>
+        </div>
+        <button>Continue</button>
+      </div>
+    </body>
   );
 }
 
